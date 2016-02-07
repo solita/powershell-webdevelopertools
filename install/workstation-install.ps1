0bea5dfe19202359e375da0bcac9c7d74a1e0c24
Joona Immonen <joona.immonen@solita.fi>
2016-02-07 15:06:49 +0200

    Added few software to workstation installation script and reorganized it

--- install/workstation-install.ps1
+++ install/workstation-install.ps1
@@ -10,6 +10,7 @@ $features = @(
 	"IIS-ISAPIExtensions" 
 	"IIS-NetFxExtensibility" 
 	"IIS-ASPNET"
+	"Microsoft-Hyper-V-All"
 )
 foreach ($task in $features) {
 	Enable-WindowsOptionalFeature -Online -FeatureName $task
@@ -17,29 +18,33 @@ foreach ($task in $features) {
 
 
 # ### Install ### #
-cinst putty -y
-cinst 7zip -y
-cinst spotify -y
-cinst grepwin -y
-cinst conemu -y
-cinst fiddler4 -y
-
 # Web
 cinst firefox -y
 cinst googlechrome -y
+cinst fiddler4 -y
 
 # Powershell modules
 cinst powertab -y # you need to Import-Module PowerTab to configure this
+cinst poshgit -y
 
 # Version control and nuget packages
 cinst SourceTree -y
 cinst gitextensions -y
 cinst kdiff3 -y
-cinst poshgit -y
 cinst Nuget.commandline
 
+# Misc 
+cinst putty -y
+cinst 7zip -y
+cinst spotify -y
+cinst grepwin -y
+cinst conemu -y
+cinst winscp -y
+cinst keepass -y
+
 # Languages
-cinst DotNet4.6 -y
+cinst DotNet4.6.1 -y
+cinst netfx-4.6.1-devpack -y
 cinst nodejs.install -y
 cinst ruby -y
 cinst npm -y
@@ -47,6 +52,7 @@ cinst npm -y
 # IDE
 cinst sublimetext3 -y
 cinst notepadplusplus -y
+cinst linqpad5 -y
 # Mount visual studio image from share and run it with (for unmanned installation of vs2015)
 # .\vs_professional.exe "PathToFile\AdminDeployment.xml" /quiet /norestart
 # after visual studio installation install resharper 9.2
@@ -62,6 +68,3 @@ c:\Windows\Microsoft.NET\Framework\v4.0.30319\aspnet_regiis.exe -i
 cinst nuget.commandline -y
 nuget.exe sources add -Name "Solita public" -Source "https://www.myget.org/F/solita-episerver/"
 nuget.exe sources add -Name "EPiServer" -Source "http://nuget.episerver.com/feed/packages.svc"
-
-#other stuff
-cinst keepass -y
